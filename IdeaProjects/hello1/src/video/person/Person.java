c25e703efe257d5630afeb5c9a12eb6f79275535
x <ivideo@i.ua>
2016-07-20 00:18:26 +0300

    test commit

---
+++ IdeaProjects/hello1/src/video/person/Person.java
@@ -0,0 +1,29 @@
+package video.person;
+/**
+ * Created by x on 19.07.2016.
+ */
+ public class Person
+{
+        public String name;
+        public int age;
+public static int count = 0;
+    static
+    {
+        System.out.println ("Расчет количества сотрудников");
+        count = 0;
+    }
+    public Person(String name, int age)
+{
+    this.name = name;
+    this.age = age;
+    count++;
+}
+    public Person(int age)
+    {
+        this("New Person", age);
+
+    }
+        public void show() {
+            System.out.printf("%s - %d лет\n", name, age);
+        }
+}
