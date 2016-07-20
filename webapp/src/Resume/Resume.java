86da2674f0195710a50ecc4fe3848ec283d30696
x <ivideo@i.ua>
2016-07-20 21:37:43 +0300

    car samples

---
+++ webapp/src/Resume/Resume.java
@@ -0,0 +1,12 @@
+package Resume;
+
+import java.util.Collection;
+
+/**
+ * Created  on 20.07.2016.
+ */
+public class Resume {
+    String fullName;
+    String[] projects;
+    Collection<String> works;
+}
